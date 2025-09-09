23d449e5fe311fa0027bfab47c7f2bef7101b394
denizzozupek <denizerenozupek@gmail.com>
2025-09-09 17:35:30 +0300

    Update

--- config.py
+++ config.py
@@ -3,11 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class Config:
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    db_path = os.getenv("DATABASE_PATH")
-    
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{db_path}"
+class Config:   
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAX_CONTENT_LENGTH = 5 * 1024 * 1024
     SECRET_KEY = os.getenv("SECRET_KEY")
