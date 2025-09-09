afef6f0c5d3dd7157db8cf03a2c316bab93a9fa9
denizzozupek <denizerenozupek@gmail.com>
2025-09-09 18:08:26 +0300

    Updated

--- config.py
+++ config.py
@@ -3,12 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class Config:   
+class Config:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAX_CONTENT_LENGTH = 5 * 1024 * 1024
     SECRET_KEY = os.getenv("SECRET_KEY")
-    
+
     # Upload settings
     UPLOAD_FOLDER = 'app/static/uploads'
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
