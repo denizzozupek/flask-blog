25f1bb6c7da945e8394d4e1850ad9736a2822ad8
denizzozupek <denizerenozupek@gmail.com>
2025-09-12 18:22:07 +0300

    Update category-toggle.js

--- app/static/js/category-menu-toggle.js
+++ app/static/js/category-menu-toggle.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     const dropdown = document.querySelector(".nav-links .dropdown");
     const dropdownMenu = dropdown.querySelector(".dropdown-menu");
+    const dropdownLink = dropdown.querySelector("a"); // Kategoriler linki
 
     // Masaüstü için hover davranışı
     dropdown.addEventListener("mouseenter", function() {
-        if (window.innerWidth > 768) { // mobil değilse
+        if (window.innerWidth > 768) {
             dropdownMenu.style.display = "block";
         }
     });
@@ -15,9 +16,9 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    // Mobil için toggle (click)
-    dropdown.addEventListener("click", function(e) {
-        if (window.innerWidth <= 768) { // sadece mobilde çalışsın
+    // Mobil için toggle
+    dropdownLink.addEventListener("click", function(e) {
+        if (window.innerWidth <= 768) {
             e.preventDefault(); // linke gitmeyi engelle
             dropdownMenu.style.display =
                 dropdownMenu.style.display === "block" ? "none" : "block";
