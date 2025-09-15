cca6154af85299cae24f7314675cf3a2010900e5
denizzozupek <denizerenozupek@gmail.com>
2025-09-15 19:02:28 +0300

    Update minor changes

--- run.py
+++ run.py
@@ -23,5 +23,4 @@ if __name__ == '__main__':
             db.session.commit()
     
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True)
-    #    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
